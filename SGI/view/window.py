344bf30411e50d01b7b7d384dc1220fb2aaf81f2
JVSutter <joaovsutter21@gmail.com>
2025-03-16 14:48:39 -0300

    Rudimentos: janela que abre, pastas, classes iniciais

---
+++ SGI/view/window.py
@@ -0,0 +1,8 @@
+class Window:
+    """Classe que representa a janela de visualização."""
+
+    def __init__(self, x_min, x_max, y_min, y_max):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
